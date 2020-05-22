86958a33d8a1d14faaab8eb77c78f4fa81bcbce5
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-05-22 11:13:04 +0100

    useEffect to console.log

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function TodoItem() {
   let [done, setDone] = useState(false);
-  let [text, setText] = useState("do stuff");
+  let [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log(`${text} is ${done ? 'DONE' : 'NOT DONE'}`)
+  }, [done, text])
 
   return (
     <div className="todoItem">
-      <input type="checkbox" checked={done} onClick={() => setDone(d => !d)} />
-      <span class={done ? 'done' : 'todo'}>{ text }</span>
+      <input
+        type="checkbox"
+        checked={done}
+        onChange={(e) => setDone(e.target.checked)}
+      />
+      <input
+        className={done ? "done" : "todo"}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onClick={(e) => e.target.setSelectionRange(0, e.target.value.length)}
+        placeholder="do stuff"
+      />
     </div>
   );
 }
